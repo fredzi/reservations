f970a6aafc58d4a9743970e149583bba6facbbc6
fredzi <maprzepiorka@gmail.com>
2015-09-29 19:31:52 +0200

    zmienione table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('name');
+            $table->string('city');
+            $table->string('street');
+            $table->string('postcode');
+            $table->string('www');
             $table->rememberToken();
             $table->timestamps();
         });
