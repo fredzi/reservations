bb08a6580421fe504d9824ce424a294a82dd7478
fredzi <maprzepiorka@gmail.com>
2015-10-03 19:50:04 +0200

    dfsdf

---
+++ app/Http/Requests/CreateMovie.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class CreateMovie extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'original_title' => 'required',
+            'time' => 'required|integer',
+            'describtion' => 'required|max:1000'
+        ];
+    }
+}
---
+++ resources/views/forms/buttons.blade.php
@@ -0,0 +1,9 @@
+
+<div class="box-footer">
+    <a href="{{ action($submit_action) }}" type="submit" class="btn btn-default pull-right">
+        Anuluj
+    </a>
+    <button type="submit" class="btn btn-info pull-left">
+        Dodaj
+    </button>
+</div>
